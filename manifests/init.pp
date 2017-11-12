d42049f8da9ef9bbcfb421548970fed249af6806
EmreJM <emre_jm@live.com>
2017-11-12 17:10:18 +0000

    makes my laptop a slave

---
+++ manifests/init.pp
@@ -0,0 +1,31 @@
+class makeslave {
+	
+	package { 'puppet':
+		ensure => 'installed',
+	}
+
+	file { '/etc/puppet/puppet.conf':
+		content => template('makeslave/puppet.conf.erb'),
+		notify => Service['puppet'],
+		require => Package['puppet'],
+
+	}
+    
+	service { 'puppet':
+		ensure  => 'true',
+		enable  => true,
+		require => Package['puppet'],
+	}
+
+	exec { 'puppet-agent':
+		path => ["/bin", "/usr/bin"],
+		command => "puppet agent -tdv",
+		require => Service["puppet"],
+	}
+	
+	
+}
+
+
+
+
